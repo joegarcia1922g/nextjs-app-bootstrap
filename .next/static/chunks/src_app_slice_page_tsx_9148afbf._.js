dae08d78876b7605aa49a63b49cc9ee1758ad286
joegarcia1922g <joegarcia2392@hotmail.com>
2025-05-14 16:43:09 -0600

    Add .next/static/chunks/src_app_slice_page_tsx_9148afbf._.js 1747262590776

---
+++ .next/static/chunks/src_app_slice_page_tsx_9148afbf._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_three_examples_jsm_loaders_STLLoader_223f7dbf.js",
+  "static/chunks/node_modules_three_build_three_core_df0bde7e.js",
+  "static/chunks/node_modules_three_build_three_module_230a9f89.js",
+  "static/chunks/node_modules_three_build_three_module_90b4155f.js",
+  "static/chunks/node_modules_react-reconciler_25c8ec02._.js",
+  "static/chunks/node_modules_@react-three_fiber_dist_5b27da2b._.js",
+  "static/chunks/node_modules_three-stdlib_controls_121d7b83._.js",
+  "static/chunks/node_modules_troika-three-text_dist_troika-three-text_esm_2702bf60.js",
+  "static/chunks/node_modules_1e5f2f3e._.js",
+  "static/chunks/src_3f2a0734._.js"
+],
+    source: "dynamic"
+});
